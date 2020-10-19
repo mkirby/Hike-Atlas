14be533defd16af3c657993ae8bd44378d655018
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-19 19:44:51 -0400

    created initial user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,50 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:errors] = @user.errors.full_messages
+            redirect_to new_user_path
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            flash[:errors] = @user.errors.full_messages
+            redirect_to edit_user_path(@user)
+        end
+    end
+
+    def destroy
+        @user.delete
+    end
+
+    private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:email, :password, :phone, :first_name, :last_name, :e_contact_name, :e_contact_email, :e_contact_phone)
+    end
 end
