163fb0096a6037d1097062829cca7e0542a938b5
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-19 20:02:12 -0400

    added trail#index and #show to controller

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,2 +1,14 @@
 class TrailsController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    private
+
+    def trail_params
+        params.require(:trail).permit(:api_id, :name, :summary, :difficulty, :stars, :location, :img_sq_small, :img_medium, :length, :ascent, :high, :condition_status, :condition_details, :condition_date, :url)
+    end
 end
