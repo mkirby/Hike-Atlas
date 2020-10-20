21c32986a2b94427905f33e0ff0c59972c2fb25d
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-19 23:12:54 -0400

    updated user custom methods and added notes on edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,26 @@ class User < ApplicationRecord
     has_many :hikes, through: :user_hikes
     has_secure_password
 
+    def past_hikes
+        #needs to select only hikes that have past
+        #work in progress
+        #self.hikes.select { |hike| hike.end_date}
+    end
+
     def total_miles
+        #needs to only pull from past hikes
         self.hikes.map{ |hike| hike.trail.length }.sum
     end
 
     def highest_peak
+        #returns nil if they've been on no hikes
+        #needs to only pull from past hikes
         self.hikes.map { |hike| hike.trail.high }.max
     end
 
     def longest_ascent
+        #returns nil if they've been on no hikes
+        #needs to only pull from past hikes
         self.hikes.map { |hike| hike.trail.ascent }.max
     end
 end
