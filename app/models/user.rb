2675eb87ed9f5c14129b5554b67eeb63dc2c8c67
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-19 22:57:36 -0400

    added custom user methods to display users stats

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
     has_many :user_hikes
     has_many :hikes, through: :user_hikes
     has_secure_password
+
+    def total_miles
+        self.hikes.map{ |hike| hike.trail.length }.sum
+    end
+
+    def highest_peak
+        self.hikes.map { |hike| hike.trail.high }.max
+    end
+
+    def longest_ascent
+        self.hikes.map { |hike| hike.trail.ascent }.max
+    end
 end
