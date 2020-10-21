be62537401df325cec02d32d1c220ad1cb6a5ea9
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-21 15:53:01 -0400

    added user authentication and log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'users#index'
+
+  get '/sessions/new_login', to: 'sessions#new_login', as: 'new_login'
+  post '/sessions/new_login', to: 'sessions#login'
+  delete '/sessions/logout', to: 'sessions#logout', as: 'logout'
+
   resources :hike_items
   resources :user_hikes
   resources :items
@@ -8,13 +13,4 @@ Rails.application.routes.draw do
   resources :trails
 	resources :users
 
-  #get '/', to: 'sessions#new_login', as: 'new_login'
-	# post '/', to: 'sessions#create_login'
-	# delete '/sessions/logout', to: 'sessions#logout', as: 'logout'
-  
-	# get '/login' => 'sessions#new' #get the login form?
-  # post '/login' => 'sessions#create'  #verify crendtials to log in
-  # post '/logout' => 'sessions#destroy'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
