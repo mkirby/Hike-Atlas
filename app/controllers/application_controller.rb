82fe68e3ed0d06f5cf18aa345e053c31eb882fc1
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-22 14:57:04 -0400

    fixed root and User index page not knowing current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-	before_action :authorized?
+	before_action :authorized?, :logged_in_user?
 
 	def logged_in_user?
 			@current_user = User.find_by(id: session[:user_id])
