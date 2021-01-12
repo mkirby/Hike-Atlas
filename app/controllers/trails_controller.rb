524216fd94d7f8266308200b07f070dc0f598217
Matthew Kirby <matt.kirby@yahoo.com>
2021-01-12 11:25:21 -0500

    finished app for presentation

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -20,7 +20,7 @@ class TrailsController < ApplicationController
     private
 
     def trail_params
-        params.require(:trail).permit(:api_id, :name, :summary, :difficulty, :stars, :location, :img_sq_small, :img_medium, :length, :ascent, :high, :condition_status, :condition_details, :condition_date, :url)
+        params.require(:trail).permit(:api_id, :name, :summary, :difficulty, :stars, :location, :img_sq_small, :img_medium, :length, :ascent, :high, :condition_status, :condition_details, :condition_date, :url, :latitude, :longitude)
     end
     
     def find_trail
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_214734) do
+ActiveRecord::Schema.define(version: 2020_11_08_212631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 2020_10_19_214734) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "user_hikes", force: :cascade do |t|
